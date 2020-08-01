09ce1b6894f1333b221265521c49628b1f64e028
Ryan Spoone <ryanspoone@gmail.com>
2020-07-31 23:34:25 -0500

    Update TEMPLATE_GITHUB_REPOSITORY

--- on-template.js
+++ on-template.js
@@ -5,7 +5,7 @@ const { writeFileSync, readFileSync, unlinkSync } = require('fs');
 const package = require('./package.json');
 
 const TEMPLATE_AUTHOR = 'Ryan Spoone';
-const TEMPLATE_GITHUB_REPOSITORY = 'ryanspoone/javascript-template';
+const TEMPLATE_GITHUB_REPOSITORY = 'ryanspoone/nodejs-template';
 const { GITHUB_REPOSITORY, GITHUB_ACTOR } = process.env;
 
 if (GITHUB_REPOSITORY === TEMPLATE_GITHUB_REPOSITORY) {
